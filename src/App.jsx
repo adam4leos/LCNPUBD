1da51c734297f098e07c40be8722fae20fa3285a
adam4leos <adam.leos@pm.me>
2019-09-14 15:06:50 +0300

    components inited

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import FAQ from './components/FAQ';
+
 const AppEl = styled.div`
   text-align: center;
-`;
-
-const Header = styled.header`
-  background-color: #282c34;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
 `;
 
 function App() {
   return (
     <AppEl>
-      <Header>
-        <ul>
-          <li>
-            Main
-          </li>
-          <li>
-            Media
-          </li>
-          <li>
-            Contacts
-          </li>
-          <li>
-            FAQ
-          </li>
-        </ul>
-      </Header>
+      <FAQ />
     </AppEl>
   );
 }
---
+++ src/components/FAQ.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const FAQ = () => (
+  <div>
+    bla
+  </div>
+);
+
+export default FAQ;
