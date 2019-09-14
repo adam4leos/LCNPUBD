530226e5646e33b9ab15b9009e197f33464d40f3
adam4leos <adam.leos@pm.me>
2019-09-14 17:44:46 +0300

    faq done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,29 +5,68 @@ import { Link } from 'react-router-dom';
 const HeaderEl = styled.div`
   display: flex;
   justify-content: center;
+  margin-bottom: 50px;
 `;
 
-const Navigation = styled.div`
+const Navigation = styled.ul`
   display: flex;
   justify-content: space-around;
   width: 100%;
+  list-style: none;
+  background: #2c4890;
+  margin: 0;
+  padding: 0;
+`;
+
+const Item = styled.li`
+  background: #2c4890;
+  color: #fff;
+  text-align: center;
+  width: 25%;
+  font-size: 0;
+  transition: background 300ms;
+
+  &:hover {
+    background: #4060b4;
+  }
+`;
+
+const LinkEl = styled(Link)`
+  display: inline-block;
+  padding: 20px;
+  color: #fff;
+  text-decoration: none;
+  width: 100%;
+  font-size: 20px;
+  box-sizing: border-box;
+  text-transform: uppercase;
+  font-weight: bold;
+  transition: opacity 300ms;
+
+  &:visited {
+    color: #fff;
+  }
+
+  &:hover {
+    opacity: .8;
+  }
 `;
 
 const Header = () => (
   <HeaderEl>
     <Navigation>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/contacts/">Contacts</Link>
-      </li>
-      <li>
-        <Link to="/media/">Media</Link>
-      </li>
-      <li>
-        <Link to="/faq/">FAQ</Link>
-      </li>
+      <Item>
+        <LinkEl to="/">Home</LinkEl>
+      </Item>
+      <Item>
+        <LinkEl to="/contacts/">Contacts</LinkEl>
+      </Item>
+      <Item>
+        <LinkEl to="/media/">Media</LinkEl>
+      </Item>
+      <Item>
+        <LinkEl to="/faq/">FAQ</LinkEl>
+      </Item>
     </Navigation>
   </HeaderEl>
 );
